f32cd5ce1f289e996da44ff14715d7b5feecfad4
aScHEABL <22256864+aScHEABL@users.noreply.github.com>
2024-03-20 17:22:07 +0800

    wip: marco recording feature #0

--- src/renderer/src/components/InitialState.tsx
+++ src/renderer/src/components/InitialState.tsx
@@ -1,32 +1,67 @@
-import { ActionIcon, Flex, Drawer } from '@mantine/core';
+import { useState, useEffect } from 'react';
+
+import { ActionIcon, Flex, Drawer, Modal, Button, Kbd, Center } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 
-import helldiversLogo from '../assets/images/helldivers-logo.webp'
+import helldiversLogo from '../assets/images/helldivers-logo.webp';
 import { IoMdAdd } from "react-icons/io";
 import { IoSettingsOutline } from "react-icons/io5";
 
 
 export default function InitialState(): JSX.Element {
+    const [pressedKeys, setPressedKeys] = useState<string[]>([]);
+    const [isRecording, setIsRecording] = useState(false);
     const [openedSettings, { open: openSettings, close: closeSettings }] = useDisclosure(false);
+    const [openedAddMarcoMenu, { open: openAddMarcoMenu, close: closeAddMarcoMenu }] = useDisclosure(false);
 
     const sendMarco = () => {
         // window.electron.ipcRenderer.send('set-marco', { oribitalAirbrust: "D+D+D" })
-        window.electron.ipcRenderer.send('set-marco', { eagleClusterBomb: "W+D+S+S+D" })
+        // window.electron.ipcRenderer.send('set-marco', { eagleClusterBomb: "W+D+S+S+D" });
+        setIsRecording(true);
     }
+
+    useEffect(() => {
+        const handlekeyDown = (event) => {
+            if (isRecording === true && !pressedKeys.includes(event.key)) {
+                setPressedKeys((prevPressedKeys) => [...prevPressedKeys, event.key]);
+                if (pressedKeys.length >= 2) setIsRecording(false);
+            }
+        }
+    })
 
     return (
         <>  
             <Drawer opened={openedSettings} onClose={closeSettings} title="Settings">
 
             </Drawer>
-            
+
+            <Modal opened={openedAddMarcoMenu} onClose={closeAddMarcoMenu} title="" centered
+            pos="absolute" style={{
+                left: 0,
+            }}
+            >   
+                { isRecording === true ? 
+                <p className="tip">
+                    Press any <code>key</code> to bind shortcuts
+                </p>
+                :
+                null }
+
+                {/* <Center>
+                    <Button variant="outline" color="cyan">Record</Button>
+                </Center> */}
+
+                
+
+                <Flex justify="flex-end" gap="md">
+                    <Button onClick={closeAddMarcoMenu}>Save</Button>
+                </Flex>
+            </Modal>
+
             <img alt="logo" className="logo" src={helldiversLogo} />
             <div className="creator">Developed by Defalt</div>
-            {/* <p className="tip">
-                Press any <code>key</code> to bind shortcuts
-            </p> */}
             <Flex gap="md">
-                <ActionIcon variant='outline' color='gray' radius="xl" aria-label="add-button" onClick={sendMarco}>
+                <ActionIcon variant='outline' color='gray' radius="xl" aria-label="add-button" onClick={() => { sendMarco(); openAddMarcoMenu(); }}>
                     <IoMdAdd />
                 </ActionIcon>
                 <ActionIcon variant='outline' color='gray' radius="xl" aria-label="settings-button" onClick={openSettings}>
