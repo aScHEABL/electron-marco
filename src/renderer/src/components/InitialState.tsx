1b91bd39840d09e86d5dd660026b3d612704734a
aScHEABL <22256864+aScHEABL@users.noreply.github.com>
2024-03-21 13:24:15 +0800

    update: record marco feature

--- src/renderer/src/components/InitialState.tsx
+++ src/renderer/src/components/InitialState.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, ReactElement, Fragment } from 'react';
 
 import { ActionIcon, Flex, Drawer, Modal, Button, Kbd, Center } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
@@ -7,6 +7,23 @@ import helldiversLogo from '../assets/images/helldivers-logo.webp';
 import { IoMdAdd } from "react-icons/io";
 import { IoSettingsOutline } from "react-icons/io5";
 
+
+function keyboardIcon(array: string[]) {
+    // array.forEach((item, index) => {
+    //     const key: string = array[index];
+    //     arrayWithIcon.push(<kbd>{key}</kbd>)
+    // })
+    return (
+        <Fragment>
+            {array.map((element, index) => (
+                <Fragment key={index}>
+                    <Kbd>{element}</Kbd>
+                    {index < array.length - 1 && " + "}
+                </Fragment>
+            ))}
+        </Fragment>
+    )
+}
 
 export default function InitialState(): JSX.Element {
     const [pressedKeys, setPressedKeys] = useState<string[]>([]);
@@ -17,17 +34,43 @@ export default function InitialState(): JSX.Element {
     const sendMarco = () => {
         // window.electron.ipcRenderer.send('set-marco', { oribitalAirbrust: "D+D+D" })
         // window.electron.ipcRenderer.send('set-marco', { eagleClusterBomb: "W+D+S+S+D" });
+    }
+
+    const handleAddMenuOpen = () => {
+        setPressedKeys([]);
+        openAddMarcoMenu();
         setIsRecording(true);
     }
 
+    const handleAddMenuClose = () => {
+        setPressedKeys([]);
+        closeAddMarcoMenu();
+        setIsRecording(false);
+    }
+
     useEffect(() => {
-        const handlekeyDown = (event) => {
+        if (pressedKeys.length >= 3) {
+            setIsRecording(false);
+        }
+
+        const handleKeyDown = (event) => {
             if (isRecording === true && !pressedKeys.includes(event.key)) {
-                setPressedKeys((prevPressedKeys) => [...prevPressedKeys, event.key]);
-                if (pressedKeys.length >= 2) setIsRecording(false);
+                if (pressedKeys.length >= 3) {
+                    return;
+                } else {
+                    setPressedKeys((prevPressedKeys) => [...prevPressedKeys, event.key]);
+                }
             }
         }
-    })
+
+        if (isRecording) {
+            document.addEventListener('keydown', handleKeyDown);
+        }
+
+        return () => {
+            document.removeEventListener('keydown', handleKeyDown);
+        };
+    }, [pressedKeys, isRecording])
 
     return (
         <>  
@@ -35,17 +78,21 @@ export default function InitialState(): JSX.Element {
 
             </Drawer>
 
-            <Modal opened={openedAddMarcoMenu} onClose={closeAddMarcoMenu} title="" centered
+            <Modal opened={openedAddMarcoMenu} onClose={handleAddMenuClose} title="" centered
             pos="absolute" style={{
                 left: 0,
             }}
             >   
                 { isRecording === true ? 
-                <p className="tip">
-                    Press any <code>key</code> to bind shortcuts
-                </p>
+                    <Fragment>
+                        <p className="tip">
+                            Press any <code>key</code> to bind shortcuts.<br />
+                            Maximum 3 keys
+                        </p>
+                        { pressedKeys.length === 0 ? null : keyboardIcon(pressedKeys) }
+                    </Fragment>
                 :
-                null }
+                    keyboardIcon(pressedKeys) }
 
                 {/* <Center>
                     <Button variant="outline" color="cyan">Record</Button>
@@ -54,14 +101,17 @@ export default function InitialState(): JSX.Element {
                 
 
                 <Flex justify="flex-end" gap="md">
-                    <Button onClick={closeAddMarcoMenu}>Save</Button>
+                    {isRecording ? 
+                    <Button onClick={() => {setIsRecording(false)}}>Stop Recording</Button>
+                    :
+                    <Button onClick={closeAddMarcoMenu}>Save</Button>}
                 </Flex>
             </Modal>
 
             <img alt="logo" className="logo" src={helldiversLogo} />
             <div className="creator">Developed by Defalt</div>
             <Flex gap="md">
-                <ActionIcon variant='outline' color='gray' radius="xl" aria-label="add-button" onClick={() => { sendMarco(); openAddMarcoMenu(); }}>
+                <ActionIcon variant='outline' color='gray' radius="xl" aria-label="add-button" onClick={() => { handleAddMenuOpen(); }}>
                     <IoMdAdd />
                 </ActionIcon>
                 <ActionIcon variant='outline' color='gray' radius="xl" aria-label="settings-button" onClick={openSettings}>
