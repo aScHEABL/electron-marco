f700377d0ebd1c0d52c2a7ad4b720e7b14f38097
aScHEABL <22256864+aScHEABL@users.noreply.github.com>
2024-03-22 17:24:11 +0800

    wip: search select with object as option

--- src/renderer/src/components/SearchableSelect.tsx
+++ src/renderer/src/components/SearchableSelect.tsx
@@ -1,19 +1,33 @@
 import { useEffect, useState } from "react";
 import { Combobox, InputBase, useCombobox } from '@mantine/core';
 
-import { selectOptions } from "@renderer/stratagem";
-
+import { selectOptions, testSelectOptions } from "@renderer/stratagem";
 
 const allSelectOptions = 
 selectOptions.reduce<string[]>((acc, group) => [...acc, ...group.options], []);
+// Expermental
+const testAllSelectOptions: string[] = testSelectOptions.flatMap((xItem) => {
+  return xItem.options.map(yItem => yItem.englishName);
+})
+// Expermental
 
 export default function SearchableSelect({ value, setValue }) {
     const combobox = useCombobox({
         onDropdownClose: () => combobox.resetSelectedOption(),
     });
 
-    // const [value, setValue] = useState<string | null>(null);
     const [search, setSearch] = useState('');
+
+    const testShouldFilterOptions = testAllSelectOptions.every((item) => item !== search);
+    const testFilteredGroups = testSelectOptions.map((group) => {
+      const testFilteredOptions = testShouldFilterOptions
+        ? group.options.filter((item) => item.englishName.toLowerCase().includes(search.toLowerCase().trim()))
+        : group.options
+
+        return { ...group, options: testFilteredOptions };
+    })
+    // Expermental
+    const testTotalOptions = testFilteredGroups.reduce((acc, group) => acc + group.options.length, 0);
 
     const shouldFilterOptions = allSelectOptions.every((item) => item !== search);
     const filteredGroups = selectOptions.map((group) => {
@@ -23,6 +37,7 @@ export default function SearchableSelect({ value, setValue }) {
 
         return { ...group, options: filteredOptions };
     })
+    // Expermental
 
     const totalOptions = filteredGroups.reduce((acc, group) => acc + group.options.length, 0);
 
@@ -41,7 +56,7 @@ export default function SearchableSelect({ value, setValue }) {
     });
 
     useEffect(() => {
-        console.log(value);
+        console.log(allSelectOptions);
     }, [value])
     return (
         <Combobox
--- src/renderer/src/stratagem.ts
+++ src/renderer/src/stratagem.ts
@@ -1,3 +1,14 @@
+interface TestSelectOption {
+    label: string;
+    options: TestOption[];
+}
+
+interface TestOption {
+    englishName: string;
+    chineseName: string;
+    keyCombinations: string;
+}
+
 export const stratagem = [
     {
         "englishName": "MG-43 Machine Gun",
@@ -32,5 +43,52 @@ export const selectOptions = [
         label: 'Orbital',
         options: ['Gatling Barrage', 'Airburst Strike', '120MM HE Barrage',
         '380MM HE Barrage', 'Walking Barrage', 'Laser Strike']
+    }
+]
+
+export const testSelectOptions: TestSelectOption[] = [
+    {
+        label: 'Weapons',
+        options: [
+            {
+                englishName: "MG-43 Machine Gun",
+                chineseName: "MG-43 機關槍",
+                keyCombinations: "saswd"
+            },
+            {
+                englishName: "Anti-Material Rifle",
+                chineseName: "反器材步槍",
+                keyCombinations: "sadws"
+            },
+            {
+                englishName: "Stalwart",
+                chineseName: "",
+                keyCombinations: "saswwa"
+            },
+            {
+                englishName: "Expendable Anti-Tank",
+                chineseName: "",
+                keyCombinations: "ssawd"
+            }]
+    },
+    {
+        label: 'Orbital',
+        options: [
+            {
+                englishName: "Gatling Barrage",
+                chineseName: "",
+                keyCombinations: "wsaww"
+            },
+            {
+                englishName: "Airburst Strike",
+                chineseName: "",
+                keyCombinations: "ddd"
+            },
+            {
+                englishName: "120MM HE Barrage",
+                chineseName: "",
+                keyCombinations: "dssasdss",
+            }
+        ]
     }
 ]
